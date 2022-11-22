681b10a6bd299b4af94c769dc94d81f4f4973603
Gregory Luneau <gregory.luneau@gmail.com>
2022-11-21 19:48:20 -0500

    api endpoint for json

---
+++ pages/api/networks.js
@@ -0,0 +1,13 @@
+import networkList from "../../resources/networks.json";
+const PUBLIC_HOST = 'https://token-resources-chi.vercel.app';
+
+Object.entries(networkList).forEach(([key, value]) => {
+  value.icon = PUBLIC_HOST + value.icon
+  console.log(`${key}: ${value.icon}`)
+})
+
+const networks = JSON.stringify(networkList)
+
+export default function handler(req, res) {
+  res.status(200).json(networks)
+}

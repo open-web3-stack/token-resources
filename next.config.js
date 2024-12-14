66aa6bd11d8f036aa015b58faeedebeaf090fe45
qwer951123 <qiuhao951@gmail.com>
2024-12-14 09:50:30 +0800

    fix: add case-insensitive routing configuration (#25)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        destination: '/:path*',
+        source: '/:path*',
+      },
+    ];
+  },
+  trailingSlash: false,
+};
 
 module.exports = nextConfig
